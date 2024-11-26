450d4a2aab8f216b26cc056868cfde70ae9630d5
yossuli <s1f102302348@iniad.org>
2024-11-27 00:45:06 +0900

    style: add, fix rule

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,28 @@
-import eslint from 'vite-plugin-eslint'
-import { VitePWA } from 'vite-plugin-pwa'
+import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 import webmanifest from './src/manifest.json';
 
 export default {
   base: './',
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
   },
   plugins: [
     eslint({
       // ESLintの設定を追加
       failOnError: false, // エラーでビルドを失敗させない
       failOnWarning: false, // 警告でビルドを失敗させない
+      overrideConfig: {
+        rules: {
+          'object-curly-spacing': 'off',
+          indent: ['warn', 2],
+        },
+      },
     }),
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
       manifest: webmanifest,
-    })
-  ]
-}
+    }),
+  ],
+};
